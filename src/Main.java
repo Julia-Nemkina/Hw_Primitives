67fcd5ea346a02f169419723d9007ec8cf7cc868
Julia Nemkina <nemkina.julia@mail.ru>
2023-07-30 18:29:41 +0300

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int bonus = 20;
+        int sum = amount / bonus;
+
+        System.out.println("Сумма миль:" + sum);
+    }
 }
